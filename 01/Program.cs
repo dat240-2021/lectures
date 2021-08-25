d1f149c21bde91e64c88e85df3401f6e4ecbe9e3
Glenn F. Henriksen <gh@justify.no>
2021-08-25 15:17:47 +0200

    Lesson 01

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using University;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.FirstName = "Fancy";
+            p.LastName = "Longpants";
+
+            Console.WriteLine(p);
+        }
+    }
+}
+namespace University {
+    class Person {
+        public string FirstName; 
+        public string LastName;
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
