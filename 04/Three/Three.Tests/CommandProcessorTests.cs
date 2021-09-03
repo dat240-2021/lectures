6d9cc6b21bbd535858f2c882eb467479b22b2b0b
Glenn F. Henriksen <gh@justify.no>
2021-09-03 08:40:36 +0200

    Added Mocks

--- 04/Three/Three.Tests/CommandProcessorTests.cs
+++ 04/Three/Three.Tests/CommandProcessorTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Three.Core;
 using Shouldly;
 using System.Collections.Generic;
+using Moq;
 
 namespace Three.Tests
 {
@@ -11,65 +12,81 @@ namespace Three.Tests
         [Fact]
         public void CommandStart_ShouldReadCommand()
         {
+            var exitCommand = new Mock<ICommand>();
+            exitCommand.Setup(c => c.Run()).Returns(false);
+
             var commands = new Dictionary<string, ICommand>
             {
-                ["exit"] = new FakeExitCommand()
+                ["exit"] = exitCommand.Object
             };
             var fakeReader = new FakeExitCommandReader();
             var commandProcessor = new CommandProcessor(fakeReader, commands);
 
             commandProcessor.Start();
 
-            fakeReader.WasCalled.ShouldBe(true);
+            exitCommand.Verify(c => c.Run(), Times.Once);
         }
 
         [Fact]
         public void CommandStart_GivenExitCommand_ShouldCallExitCommand()
         {
+            var exitCommand = new Mock<ICommand>();
+            exitCommand.Setup(c => c.Run()).Returns(false);
+
             var commands = new Dictionary<string, ICommand>
             {
-                ["exit"] = new FakeExitCommand()
+                ["exit"] = exitCommand.Object
             };
             var fakeReader = new FakeExitCommandReader();
             var commandProcessor = new CommandProcessor(fakeReader, commands);
 
             commandProcessor.Start();
 
-            ((FakeExitCommand)commands["exit"]).WasCalled.ShouldBe(true);
+            exitCommand.Verify(c => c.Run(), Times.Once);
         }
-
         [Fact]
         public void CommandStart_GivenCommandReturningFalse_ShouldTerminate()
         {
+            var exitCommand = new Mock<ICommand>();
+            exitCommand.Setup(c => c.Run()).Returns(false);
+            var continueCommand = new Mock<ICommand>();
+            continueCommand.Setup(c => c.Run()).Returns(true);
+
             var commands = new Dictionary<string, ICommand>
             {
-                ["exit"] = new FakeExitCommand(),
-                ["continue"] = new FakeContinueCommand()
+                ["exit"] = exitCommand.Object,
+                ["continue"] = continueCommand.Object
             };
             var fakeReader = new FakeCommandReader(new string[] { "exit", "continue" });
             var commandProcessor = new CommandProcessor(fakeReader, commands);
 
             commandProcessor.Start();
 
-            ((FakeExitCommand)commands["exit"]).WasCalled.ShouldBe(true);
-            ((FakeContinueCommand)commands["continue"]).WasCalled.ShouldBe(false);
+            exitCommand.Verify(c => c.Run(), Times.Once);
+            continueCommand.Verify(c => c.Run(), Times.Never);
         }
 
         [Fact]
         public void CommandStart_GivenCommandReturningTrue_ShouldContinue()
         {
+            var exitCommand = new Mock<ICommand>();
+            exitCommand.Setup(c => c.Run()).Returns(false);
+            var continueCommand = new Mock<ICommand>();
+            continueCommand.Setup(c => c.Run()).Returns(true);
+
             var commands = new Dictionary<string, ICommand>
             {
-                ["exit"] = new FakeExitCommand(),
-                ["continue"] = new FakeContinueCommand()
+                ["exit"] = exitCommand.Object,
+                ["continue"] = continueCommand.Object
             };
+
             var fakeReader = new FakeCommandReader(new string[] { "continue", "exit" });
             var commandProcessor = new CommandProcessor(fakeReader, commands);
 
             commandProcessor.Start();
 
-            ((FakeContinueCommand)commands["continue"]).WasCalled.ShouldBe(true);
-            ((FakeExitCommand)commands["exit"]).WasCalled.ShouldBe(true);
+            exitCommand.Verify(c => c.Run(), Times.Once);
+            continueCommand.Verify(c => c.Run(), Times.Once);
         }
     }
 
