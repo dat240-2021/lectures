57584dd5016387e8f5d5bdcc1949020580ccf401
Glenn F. Henriksen <gh@justify.no>
2021-09-03 11:48:04 +0200

    Uses reflection to find all available commands

--- 04/Three/Three.Core/Program.cs
+++ 04/Three/Three.Core/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Three.Core
 {
@@ -6,12 +8,26 @@ namespace Three.Core
     {
         public static void Main()
         {
-            var commands = new Dictionary<string, ICommand>
-            {
-                ["exit"] = new ExitCommand(),
-                ["add"] = new AddCommand(),
-                ["rem"] = new RemCommand(),
-            };
+            var commands = new Dictionary<string, ICommand>();
+
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
+                .Where(t => typeof(ICommand).IsAssignableFrom(t) && !t.IsInterface)
+                .ToList();
+
+            foreach(var type in types) {
+                ICommand? command = Activator.CreateInstance(type) as ICommand;
+                if (command is not null) {
+                    commands.Add(
+                        type.Name.Replace("Command", "").ToLower(),
+                        command
+                    );
+                }
+            }
+            // {
+            //     ["exit"] = new ExitCommand(),
+            //     ["add"] = new AddCommand(),
+            //     ["rem"] = new RemCommand(),
+            // };
 
             var commandProcessor = new CommandProcessor(new ConsoleCommandReader(), commands);
             commandProcessor.Start();
